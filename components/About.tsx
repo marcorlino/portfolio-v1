89a7e34ec23301e057b1fc9a9f236b162e57c340
Jenvelle <jennzaep@gmail.com>
2024-02-18 13:45:21 +0800

    about me + cv button

--- components/About.tsx
+++ components/About.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import jcena from "@/assets/jcena.jpg";
 import Layout from "./Layout";
+import Link from "next/link";
 
 function About() {
   return (
@@ -16,18 +17,30 @@ function About() {
           ></Image>
         </div>
 
-        <div className="text-2xl text-justify w-[600px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum
-          tellus quis elit dictum molestie. Ut tempus sed lectus sed tempus.
-          Integer iaculis facilisis enim, eget dignissim neque varius eget.
-          Donec metus sapien, ornare eu turpis id, rhoncus tempor elit. Etiam eu
-          dolor mollis, porttitor ex sit amet, scelerisque massa. Vestibulum
-          sodales in velit a iaculis. Ut pulvinar tristique egestas. Nullam
-          egestas eu felis quis semper. Fusce faucibus tristique cursus. Vivamus
-          convallis congue purus quis ornare. Aliquam viverra eleifend
-          ullamcorper.
+        <div className="text-xl text-justify w-[600px]">
+        <p>Welcome to my web portfolio! I'm <span className="text-orange-400">Marc Angelo Orlino</span>, a passionate individual who holds a degree in Bachelor of Science in Information Technology, with a focused specialization in Web and Mobile Application Development.</p>
+        <p className="mt-4">Throughout my academic journey I've acquired a solid foundation in various technologies, showcasing expertise in programming languages such as <span className="text-orange-400">.NET Core, Angular, and PHP</span>. My commitment to delivering exceptional user experiences extends to my proficiency in crafting responsive and visually appealing interfaces, utilizing tools like Tailwind CSS.</p>
+        <p className="mt-4">As an aspiring full-stack developer, I am dedicated to continuous learning and innovation in the ever-evolving world of technology</p>
+        <div className="flex items-center justify-center pt-5">
+        <div className="relative">
+            <span className="absolute bg-orange-500 top-1 w-full bottom-[-5px] rounded-md -left-2">
+              &nbsp;
+            </span>
+            <Link href={"#"}>
+              <button className="rounded-md -translate-x-3 hover:-translate-x-2 hover:translate-y-1 p-2 bg-white text-black font-bold text-2xl px-6 hover:border-2 border-white duration-200 ease-out">
+                View CV
+              </button>
+            </Link>
+          </div>
+          </div>
         </div>
+
+        
+          
+       
+        
       </div>
+      
     </Layout>
   );
 }
